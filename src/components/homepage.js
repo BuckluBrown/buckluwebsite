cda19d55f06c949695be8936c3138151970d3f6a
suppixie <mamthapatalay6@gmail.com>
2024-04-12 01:35:14 +0530

    changed layout

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,40 +1,39 @@
-import chocbg1 from '../assets/chocbg1.gif';
+import mascot2 from '../assets/chocdrink-mascot.png';
+import browntypo  from '../assets/brown typo.png';
 import '../style/homepage.css';
-import insta from '../assets/instagram.png';
-import mascot from '../assets/mascot.png';
+// import insta from '../assets/instagram.png';
+import bg from '../assets/bg.jpg';
 function Homepage(){
     return(
-        <>
+        <div className='container'>
         <div className="landing-page">
-            <div className='vertical-text'>
-            <h1>B<br/>R<br/>O<br/>W<br/>N</h1></div>
-            <div className='video-container'> 
-            <img className="bg1" src={chocbg1} autoPlay alt="chocolatebg"></img>
-            <h1 className='heading'> Bucklu</h1>
+           
+            <div className='mascot-container'> 
+            <img className="mascot2" src={mascot2} alt="chocdrink-mascot"></img>
+            <img src={bg} className='bg' alt='bg'/>
+            <div className='brown-typo'>
+                <img className='brown-typo-text1' src={browntypo} alt='brown-text'></img>
+                <h1 className='brown-typo-text2'>Hot Chocolate with Jaggery</h1>
+            </div>
             </div>
         </div>
         <div className='content'>
-            
+        
             <div className='block1'>
-                <img className='mascot' src={mascot} alt='mascot'/>
+                <h1>MIX</h1>
             </div>
             <div className='block2'>
-            <h3> Hit the instagram <br/> to know more</h3>
-
+                <h1> SIP</h1>
             </div>
             <div className='block3'>
-            <img className='insta-logo' src={insta} alt="insta"/>
-
-            </div>
-            <div className='block4'>
-                <h2>Coming Soon!</h2>
+                <h1>FLY</h1>
             </div>
 
         </div>
         <footer>
-            <h3>Contact Us</h3>
+            <h3>Send a special message to your friend!</h3>
         </footer>
-         </>
+        </div>
     )
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,13 @@
 import {Link} from 'react-router-dom';
+import insta from '../assets/instagram.png';
 import '../style/navbar.css';
 function Navbar(){
     return(
         <nav>
         {/* <h1 className='bucklu'>Bucklu</h1> */}
         <ul className='nav'>
-        <li><Link to="/" className='page-items'>Home</Link></li>
-        <li><Link to="/About" className='page-items'>About us</Link></li>
+        <li><Link to="/" className='page-items'><img className='insta' src={insta} alt='insta'/></Link></li>
+        <li><Link to="/About" className='page-items'>About<br/>Brown</Link></li>
         </ul>
         </nav>
     )
